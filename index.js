dc706a5fe306d14c576a325bfb89f75ffa485948
ARVAZAKI <arvazf09@gmail.com>
2024-04-27 12:00:21 +0700

    update dokumentasi

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({ extended:true}))
 
 app.get('/', activityController.getData)
 app.post('/tambah', activityController.createData)
-app.post('/edit/(:id)', activityController.updateData)
+app.put('/edit/(:id)', activityController.updateData)
 app.get('/delete/(:id)', activityController.deleteData)
 
 app.listen(4000, () => {
